487158defa46dfdcbaf4aa1263886b2b134d1e1c
jose <jmvr37@hotmail.com>
2022-01-05 22:30:22 -0800

    add test text to profile file

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,7 +14,7 @@ const Profile = () =>{
                   <div class="text-white p-3 text-center inline-flex items-center justify-center w-20 h-20 mb-5 shadow-lg rounded-full bg-lightBlue-400 animate-pulse">
                   <img alt="..." src={Keyboard} class="lg:w-16 h-16"/>
                   </div>
-                  <h6 class="text-3xl font-mono">Hi, I'm Jose </h6>
+                  <h6 class="text-3xl font-mono">Hi, I'm Jose test 2 </h6>
                   <p class="sm:text-sm mt-2 mb-4 font-light md:text-2xl mt-2 mb-4">
                   Since the beginning of my journey as a full stack developer 2 years ago, I've done over 10 projects using different technologies, such as Javascript, php, and ReactJS.
                   I have worked with talented people to develop digital products for a few non-profit organizations. A self-taught UI/UX designer with a natural curiosity,
